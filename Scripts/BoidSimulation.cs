d07ddbd4b510cdfe9b1bdcee3deacea0ee751b99
Manos Miras <manosmiras@outlook.com>
2024-12-22 14:22:33 +0200

    boundaries

--- Scripts/BoidSimulation.cs
+++ Scripts/BoidSimulation.cs
@@ -5,26 +5,27 @@ namespace Boids.Scripts;
 
 public partial class BoidSimulation : Node
 {
-    public BoidComponent[] Boids;
     [Export] private float _maxSpeed = 2.0f;
     [Export] private float _maxForce = 0.03f;
-    [Export] private float _desiredSeparation = 25.0f;
-    [Export] private float _neighborDistance = 50f;
-    [Export] private float _turnFactor = 20.0f;
-    
+    [Export] private float _desiredSeparation = 5.0f;
+    [Export] private float _neighborDistance = 20f;
     [Export] private int _flockSize = 100;
-    private Vector3 _minBounds = new(-25f, -25f, -25f);
-    private Vector3 _maxBounds = new(25f, 25f, 25f);
+    [Export] private float _boundaryThreshold = 10f;
+    [Export] private float _boundaryForceFactor = 5f;
+    [Export] private Vector3 _minBounds = new(-100f, -25f, -50f);
+    [Export] private Vector3 _maxBounds = new(100f, 25f, 50f);
+    
+    private BoidComponent[] _boids;
     private Node3D[] _boidNodes;
 
     public override void _Ready()
     {
-        Boids = new BoidComponent[_flockSize];
+        _boids = new BoidComponent[_flockSize];
         _boidNodes = new Node3D[_flockSize];
         var boidScene = GD.Load<PackedScene>("res://Boid.tscn");
         for (var i = 0; i < _flockSize; i++)
         {
-            Boids[i] = new BoidComponent
+            _boids[i] = new BoidComponent
             {
                 Position = new Vector3(
                     (float)GD.RandRange(_minBounds.X, _maxBounds.X),
@@ -36,7 +37,7 @@ public partial class BoidSimulation : Node
             
             var boidNode = (Node3D)boidScene.Instantiate();
             _boidNodes[i] = boidNode;
-            boidNode.Position = Boids[i].Position;
+            boidNode.Position = _boids[i].Position;
             AddChild(boidNode);
         }
     }
@@ -49,37 +50,37 @@ public partial class BoidSimulation : Node
     
     private void Simulate(float delta)
     {
-        Parallel.For(0, Boids.Length, i =>
+        Parallel.For(0, _boids.Length, i =>
         {
-            Boids[i].Velocity += CalculateVelocities(i) * delta;
-            Boids[i].Position += Boids[i].Velocity;
+            _boids[i].Velocity += CalculateVelocities(i) * delta;
+            _boids[i].Position += _boids[i].Velocity;
         });
     }
     
     private void UpdateNodes()
     {
-        for (var i = 0; i < Boids.Length; i++)
+        for (var i = 0; i < _boids.Length; i++)
         {
-            _boidNodes[i].Position = Boids[i].Position;
-            var movingTowards = Boids[i].Position - Boids[i].Velocity;
-            if(movingTowards != Vector3.Zero)
-                _boidNodes[i].LookAt(movingTowards, Vector3.Up);
+            _boidNodes[i].Position = _boids[i].Position;
+            var target = _boids[i].Position - _boids[i].Velocity;
+            if(target !=  _boidNodes[i].Position)
+                _boidNodes[i].LookAt(target, Vector3.Up);
         }
     }
 
     private Vector3 CalculateVelocities(int currentBoidIndex)
     {
-        var currentBoid = Boids[currentBoidIndex];
+        var currentBoid = _boids[currentBoidIndex];
         var separationVelocity = Vector3.Zero;
         var separationCount = 0;
         var alignmentVelocity = Vector3.Zero;
         var alignmentCount = 0;
         var cohesionVelocity = Vector3.Zero;
         var cohesionCount = 0;
-        for(var j = 0; j < Boids.Length; j++)
+        for(var j = 0; j < _boids.Length; j++)
         {
             if (currentBoidIndex == j) continue;
-            var otherBoid = Boids[j];
+            var otherBoid = _boids[j];
             var distance = currentBoid.Position.DistanceTo(otherBoid.Position);
             
             AccumulateVelocity(distance, _desiredSeparation, currentBoid, otherBoid, ref separationVelocity, ref separationCount);
@@ -93,7 +94,9 @@ public partial class BoidSimulation : Node
             cohesionVelocity /= cohesionCount;
             cohesionVelocity = Seek(cohesionVelocity, currentBoid);
         }
-        return separationVelocity + alignmentVelocity + cohesionVelocity;
+
+        var boundaryVelocity = CalculateBoundaryVelocity(currentBoid);
+        return separationVelocity + alignmentVelocity + cohesionVelocity + boundaryVelocity;
     }
 
     private void AccumulateVelocity(
@@ -135,5 +138,49 @@ public partial class BoidSimulation : Node
         var steer = desired - currentVel;
         steer = steer.LimitLength(_maxForce);
         return steer;
+    }
+    
+    private Vector3 CalculateBoundaryVelocity(BoidComponent boid)
+    {
+        var desired = Vector3.Zero;
+        // Check each axis and see if we're within threshold of a boundary
+        // If so, set 'desired' velocity pointing back in
+        if (boid.Position.X < _minBounds.X + _boundaryThreshold)
+        {
+            desired.X = _maxSpeed;
+        }
+        else if (boid.Position.X > _maxBounds.X - _boundaryThreshold)
+        {
+            desired.X = -_maxSpeed;
+        }
+
+        if (boid.Position.Y < _minBounds.Y + _boundaryThreshold)
+        {
+            desired.Y = _maxSpeed;
+        }
+        else if (boid.Position.Y > _maxBounds.Y - _boundaryThreshold)
+        {
+            desired.Y = -_maxSpeed;
+        }
+
+        if (boid.Position.Z < _minBounds.Z + _boundaryThreshold)
+        {
+            desired.Z = _maxSpeed;
+        }
+        else if (boid.Position.Z > _maxBounds.Z - _boundaryThreshold)
+        {
+            desired.Z = -_maxSpeed;
+        }
+
+        if (desired == Vector3.Zero)
+            return Vector3.Zero;
+
+        // Convert desired direction into a steering force, similar to 'Seek'.
+        desired = desired.Normalized() * _maxSpeed;
+        var steer = desired - boid.Velocity;
+        steer = steer.LimitLength(_maxForce);
+
+        // Scale it by boundaryForceFactor so we can make it gentler if we want
+        return steer * _boundaryForceFactor;
     }
 }
