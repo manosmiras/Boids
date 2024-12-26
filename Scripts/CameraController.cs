4fd5e7b1fdf7b9017d47411bd831d7aa373d3f0b
Manos Miras <manosmiras@outlook.com>
2024-12-26 18:53:28 +0200

    floating camera

---
+++ Scripts/CameraController.cs
@@ -0,0 +1,48 @@
+using Godot;
+
+namespace Boids.Scripts;
+
+public partial class CameraController : Node3D
+{
+    [Export] private float _moveSpeed = 5f;
+    [Export] private float _mouseSensitivity = 0.15f;
+    [Export] private Camera3D _camera;
+    private Vector2 _mouseDelta;
+    
+    private float _pitch = 0f;
+    private float _yaw   = 0f;
+
+    public override void _Ready()
+    {
+        Input.MouseMode = Input.MouseModeEnum.Captured;
+    }
+
+    public override void _Input(InputEvent @event)
+    {
+        // Capture raw mouse movement
+        if (@event is InputEventMouseMotion mouseMotion)
+        {
+            if (@event is InputEventMouseMotion motion)
+            {
+                // Yaw (left/right) rotate around Y axis
+                _yaw -= motion.Relative.X * _mouseSensitivity;
+            
+                // Pitch (up/down) rotate around X axis
+                _pitch -= motion.Relative.Y * _mouseSensitivity;
+                _pitch = Mathf.Clamp(_pitch, -89f, 89f);
+
+                _camera.RotationDegrees = new Vector3(_pitch, _yaw, 0f);
+            }
+            _mouseDelta = mouseMotion.Relative;
+        }
+    }
+
+    public override void _Process(double delta)
+    {
+        var inputDirection = Input.GetVector("MoveLeft", "MoveRight", "MoveForward", "MoveBackward");
+        var upDown = Input.GetAxis("MoveDown", "MoveUp");
+        GD.Print(upDown);
+        var direction = _camera.Basis * new Vector3(inputDirection.X, upDown, inputDirection.Y).Normalized();
+        Translate(direction * _moveSpeed * (float)delta);
+    }
+}
